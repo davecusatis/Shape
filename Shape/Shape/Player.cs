0d393756998630a76f4c991937f523623e8b5b01
Zamaster <Batterybat@gmail.com>
2015-01-07 22:19:46 -0500

    Added more stuff

---
+++ Shape/Shape/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Shape
+{
+    class Player
+    {
+        public void Player()
+        {
+            Position = new Vector3(0, 0, 0);
+            Velocity = new Vector3(0, 0, 0);
+        }
+        public void Update(float t)
+        {
+            Velocity += Acceleration * t;
+            Position += (Velocity + FloorVelocity) * t;
+            Acceleration = new Vector3(0, 0, 0);
+        }
+        public void Draw()
+        {
+
+        }
+
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public Vector3 Acceleration;
+        public Vector3 FloorVelocity;
+
+    }
+}
