7adf912ebfd2664fe13bce49075f8799619f66dd
Zamaster <Batterybat@gmail.com>
2015-01-07 20:41:47 -0500

    Added shape types and began grid class

--- Shape/Shape/Shape.cs
+++ Shape/Shape/Shape.cs
@@ -8,26 +8,58 @@ namespace Shape
 {
     public partial class Grid
     {
-        interface IShape
+        class Shape
         {
-            enum State
+            protected static float VELOCITY_DAMP = 0.95f;
+            public enum State
             {
                 Moving, Stopped
             }
-            enum Direction
+            public enum Direction
             {
                 Positive, Negative
             }
 
-            Vector3 Position;
-            Vector2 Size;
-            Vector3 Velocity;
-            Vector3 Acceleration ;
-            State ShapeState;
-            Direction ShapeDirection;
+            protected Vector3 Position
+            {
+                public set
+                { 
+                    Position = value;
+                    Stop();
+                }
+                public get;
+            }
+            protected Vector3 Size;
+            protected Vector3 Velocity
+            {
+                public get;
+                private set;
+            }
+            protected Vector3 Acceleration;
+            protected State ShapeState;
+            protected Direction ShapeDirection;
 
-            List<Vector2> OccupiedSquare();
-            List<Vector2> NextSquares();
+            public List<Vector2> OccupiedSquares();
+            public List<Vector2> NextSquares();
+            public void Move(float speed) { }
+            public void Update(float t) { }
+            public void Draw();
+            public bool OccupiesSquare(Vector2 checkSquare)
+            {
+                List<Vector2> curSquares;
+                curSquares = OccupiedSquares();
+                foreach (var square in curSquares)
+                {
+                    if (square == checkSquare) return true;
+                }
+                return false;
+            }
+            public void Stop()
+            {
+                Velocity = new Vector3(0, 0, 0);
+                Acceleration = new Vector3(0, 0, 0);
+            }
+
         }
     }
 }
