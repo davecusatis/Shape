834ec0b4d662eeecc7767265f7f92eb017e10a36
davecusatis1 <davecusatis1@gmail.com>
2015-01-07 19:01:28 -0500

    big push

---
+++ Shape/Shape/Shape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Shape
+{
+    public partial class Grid
+    {
+        interface IShape
+        {
+            enum State
+            {
+                Moving, Stopped
+            }
+            enum Direction
+            {
+                Positive, Negative
+            }
+
+            Vector3 Position;
+            Vector2 Size;
+            Vector3 Velocity;
+            Vector3 Acceleration ;
+            State ShapeState;
+            Direction ShapeDirection;
+
+            List<Vector2> OccupiedSquare();
+            List<Vector2> NextSquares();
+        }
+    }
+}
