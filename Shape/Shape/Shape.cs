ac8ed1c32bf8b9f7b1731085ad4f82809ce02d87
davecusatis1 <davecusatis1@gmail.com>
2015-01-07 20:53:28 -0500

    bogus drawing code

--- Shape/Shape/Shape.cs
+++ Shape/Shape/Shape.cs
@@ -10,22 +10,6 @@ namespace Shape
     {
         interface IShape
         {
-            enum State
-            {
-                Moving, Stopped
-            }
-            enum Direction
-            {
-                Positive, Negative
-            }
-
-            Vector3 Position;
-            Vector2 Size;
-            Vector3 Velocity;
-            Vector3 Acceleration ;
-            State ShapeState;
-            Direction ShapeDirection;
-
             List<Vector2> OccupiedSquare();
             List<Vector2> NextSquares();
         }
