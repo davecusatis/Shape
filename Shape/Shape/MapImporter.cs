1872c5a7e5531ae7afa809c5a3e52f2766ef84b0
unknown <davecusatis1@gmail.com>
2015-04-13 10:38:40 -0400

    updated gitignore, initial MapImporter commit

---
+++ Shape/Shape/MapImporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Shape
+{
+    class MapImporter
+    {
+        private FileStream SourceFile;
+
+        public MapImporter()
+        {
+            string filename = @"C:\Users\yung\Documents\GitHub\Shape\Shape\Shape\Content\test.txt";
+            using(SourceFile = File.OpenRead(filename))
+            {
+
+            }
+        }
+    }
+}
